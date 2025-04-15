d52da7fd674eec2d894efeed7e8f56fc6402e864
Raihan Uddin <raihanarif778@gmail.com>
2025-04-15 14:14:31 +0600

    conflict resoleve

--- courses/index.js
+++ courses/index.js
@@ -4,7 +4,10 @@ import { buildCourseFilter } from "../utils/filterUtils";
 const express = require("express");
 const router = express.Router();
 
+// router.get("/courses",isAdmin, async (req, res) => {
+//   console.log("inside courses where i use mid isAdmin",req.cookies)
 router.get("/courses", async (req, res) => {
+  
   try {
     const {
       searchTerm = "",
@@ -31,11 +34,7 @@ router.get("/courses", async (req, res) => {
       courseCollection.countDocuments(filter),
     ]);
     res.status(200).json({
-try {
-  router.get("/courses",isAdmin, async (req, res) => {
-    console.log("inside courses where i use mid isAdmin",req.cookies)
-    const result = await courseCollection.find({}).toArray();
-    res.send({
+
       success: true,
       message: "Course data retrieved successfully",
       data: courses,
